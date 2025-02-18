abdf366ab12406ffeb3e69811c6781a59556d37d
dineshGitHub93 <dineshspachaiyappan93@gmail.com>
2025-02-18 21:41:34 +0530

    REST Api execution with pojo class

--- src/test/java/com/api/base/BaseService.java
+++ src/test/java/com/api/base/BaseService.java
@@ -2,6 +2,8 @@ package com.api.base;
 
 import static io.restassured.RestAssured.*;
 
+import com.api.models.request.LoginRequest;
+
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
@@ -9,14 +11,14 @@ import io.restassured.specification.RequestSpecification;
 public class BaseService { // wrapper for class Assured!!
 	//Base URi, Creating the REQUEST, Handling the RESPONSE
 	
-	private static final String BASE_URI = "http://localhost:3000";
+	private static final String BASE_URI = "http://64.227.160.186:8080/";
 	private RequestSpecification requestSpecification;
 	
 	public BaseService() {
 		requestSpecification = given().baseUri(BASE_URI);
 	}
 	
-	protected Response postRequest(String payload, String endpoint) {
+	protected Response postRequest(LoginRequest payload, String endpoint) {
 		return requestSpecification.contentType(ContentType.JSON).body(payload).post(endpoint);
 	}
 
--- src/test/java/com/api/tests/LoginAPITest1.java
+++ src/test/java/com/api/tests/LoginAPITest1.java
@@ -3,6 +3,7 @@ package com.api.tests;
 import org.testng.annotations.Test;
 
 import com.api.base.AuthService;
+import com.api.models.request.LoginRequest;
 
 import io.restassured.response.Response;
 
@@ -10,8 +11,10 @@ public class LoginAPITest1 {
 	
 	@Test(description = "Verify if login API is working..!")
 	public void loginTest() {
+		
+		LoginRequest loginRequest = new LoginRequest("string", "string");
 		AuthService authService = new AuthService();
-		Response response = authService.login("{\"email\": \"user@example.com\",\"password\": \"password\"}");
+		Response response = authService.login(loginRequest);
 		System.out.println(response.asPrettyString());
 	}
 	
